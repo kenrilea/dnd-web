915113665f79826ed3010400835608aedea7970c
kenrilea <kenrilea@gmail.com>
2019-11-02 13:16:40 -0400

    feat: GET characterstats

--- backend/custom_modules/character/character.js
+++ backend/custom_modules/character/character.js
@@ -2,13 +2,31 @@
 
 
 let collections = {}
-const getRequest = () => {
-    // new Promise ((resolve, reject) => {
-    //     collections.stats.find({ })
-    // })
+const getCharStats = (req) => {
+    if (!req.query) {
+        return { success: false, message: 'request query is required' }
+    }
+    const { characterId } = req.query;
+    if (!characterId) {
+        return { success: false, message: 'request query param "characterId" is required' }
+    }
+    console.log(`getting character stats for ${characterId}`)
+    const dbRequest = new Promise ((resolve, reject) => {
+        collections.characterStats.find({ characterId }).toArray( (err, result) => {
+            if(err) {
+                resolve({ success: false, error: err})
+            }
+            if(result.length < 1) {
+                resolve({ success: false, message: 'characterId does not exist'})
+            }
+            console.log(`success: recieved data for ${characterId}`)
+            resolve({ success: true, data: {...result[0]} })
+        })
+    })
+    return dbRequest;
 }
 
-const postRequest = (req) => {
+const newCharStats = (req) => {
     if (!req.body) {
         return { success: false, message: 'request body is required' }
     }
@@ -39,18 +57,23 @@ const postRequest = (req) => {
     return dbRequest;
 }
 const routes = async (app, upload, initialize) => {
-    app.get('/character', upload.none(), (req, res) => {
+    app.get('/character/stats', upload.none(), (req, res) => {
+        console.log()
+        console.log('GET: /character/stats')
         collections = initialize()
-        character.setCollections(collections)
-        getRequest();
-        res.send('character')
+        const dbResult = getCharStats(req);
+        dbResult.then((result) => {
+            res.send(result)
+            console.log('request complete')
+            console.log()
+        })
     });
     app.post('/character/stats', upload.none(), (req,res) => {
         console.log()
         console.log('POST: /character/stats')
         collections = initialize()
         console.log('recieved')
-        const dbResult = postRequest(req);
+        const dbResult = newCharStats(req);
         dbResult.then(result => {
             console.log(result)
             res.send(JSON.stringify(result))
